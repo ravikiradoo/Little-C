deb00ca01414e0eba9ed2a044e3798e662cd07c9
Chirag Garg <gargchirag19@gmail.com>
2017-04-20 23:31:21 +0530

    Add files via upload

---
+++ sample.c
@@ -0,0 +1,39 @@
+import[standard math]
+int a 
+float b
+char c
+function int first () 
+{
+int i
+read(g) 
+for(i=1;i<=5;i=i+1)
+{
+Return i
+}
+while(i<=11)
+{
+read(b)
+if(b<3)
+{
+i=i+2
+b=b+2
+}
+}
+}
+function int second ()
+{
+int j
+float k
+read(j)
+while(j<=10)
+{
+if(b>3)
+{
+for(b=1;b<=5;b=b+1)
+{
+i=i-1
+Return i
+}
+}
+}
+}
---
+++ sample_error.c
@@ -0,0 +1,12 @@
+import[standard math]
+int a 
+function int main () 
+{
+int i,j
+// Error We can not simultaneously define two integers.
+read(g) 
+for(i=1;i<=5;i=i+1)
+{
+Return i
+}
+}
